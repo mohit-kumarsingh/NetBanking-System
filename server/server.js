c7ff6c40e67755d10319f7a69a691d4bba1a0618
mayank-del <mkjam.007@gmail.com>
2022-09-10 13:18:31 +0530

    new

--- server/server.js
+++ server/server.js
@@ -8,6 +8,8 @@ const cors=require('cors');
 app.use(express.json());
 app.use(cors());
 
+const User = require('./schema/UserSchema');
+
 //MongoDB connection :TODO: MOngoURI
 mongoose.connect(process.env.DB)
 .then(()=>{
@@ -24,9 +26,33 @@ app.get('/', (req, res) => {
 });
 
 //Signup Route
-app.post('/signup', (req, res) => {
-    console.log(req.body);
+app.post('/signup', async(req, res) => {
+    //console.log(req.body);
+    try{
+        await new User(req.body).save();
+        res.status(201).send({ message: "User created successfully" });
+    }catch(e){
+        res.status(500).send({ message: "Internal Server Error" });
+    }
+
 });
+app.post("/login",async(req,res,next)=>{
+    try{
+        const user=await User.findOne({email:req.body.email})
+        
+        if(!user){
+            res.status(401);
+            return res.send({message:"No user with this email exists, Please signup"});
+           
+        }
+        if(user.password===req.body.password && user.email===req.body.email){
+            res.status(200).send({ data: token, message: "logged in successfully",user });
+            next();
+        }
+    }catch(e){
+        console.log(e);
+    }
+})
 
 const port=process.env.PORT || 5000;
 app.listen(port, ()=>{
